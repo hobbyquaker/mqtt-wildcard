c7708e5e2915e105b0bc1abe4d0580e775a50718
hobbyquaker <bastiraff@googlemail.com>
2018-05-09 22:49:27 +0200

    compare to similar modules

--- benchmark.js
+++ benchmark.js
@@ -1,83 +1,84 @@
 #!/usr/bin/env node
 
 const Benchmark = require('benchmark');
-const suite = new Benchmark.Suite;
 
 const mw = require('./index.js');
-const mw2 = require('./index-old.js');
+const mqttMatch = require('mqtt-match');
+const mqttPattern = require("mqtt-pattern").exec;
 
-suite.add('new wildcard match', function () {
-    mw('test/test/test/test', 'test/test/test/test');
-}).add('old wildcard match', function () {
-    mw2('test/test/test/test', 'test/test/test/test');
-}).add('new wildcard match +', function () {
-    mw('test/test/test/test', 'test/+/test/+');
-    mw('test/test/test/test', 'test/+/+/+');
-    mw('test/test/test/test', 'test/+/+/test');
-    mw('test/test/test/test', 'test/test/test/+');
-}).add('old wildcard match +', function () {
-    mw2('test/test/test/test', 'test/+/test/+');
-    mw2('test/test/test/test', 'test/+/+/+');
-    mw2('test/test/test/test', 'test/+/+/test');
-    mw2('test/test/test/test', 'test/test/test/+');
-}).add('new wildcard mismatch +', function () {
-    mw('test/test/test/test', 'test/+/muh/+');
-    mw('test/test/test/test', 'test/+/+');
-    mw('test/test/test/test', 'test/+/+/muh');
-    mw('test/test/test/test', 'test/test/+');
-}).add('old wildcard mismatch +', function () {
-    mw2('test/test/test/test', 'test/+/muh/+');
-    mw2('test/test/test/test', 'test/+/+');
-    mw2('test/test/test/test', 'test/+/+/muh');
-    mw2('test/test/test/test', 'test/test/+');
-}).add('new wildcard match #', function () {
-    mw('test/test/test/test', 'test/test/test/#');
-    mw('test/test/test/test', 'test/test/#');
-    mw('test/test/test/test', 'test/#');
-    mw('test/test/test/test', '#');
-}).add('old wildcard match #', function () {
-    mw2('test/test/test/test', 'test/test/test/#');
-    mw2('test/test/test/test', 'test/test/#');
-    mw2('test/test/test/test', 'test/#');
-    mw2('test/test/test/test', '#');
-}).add('new wildcard mismatch #', function () {
-    mw('test/test/test/test', 'test/test/muh/#');
-    mw('test/test/test/test', 'test/muh/#');
-    mw('test/test/test/test', 'muh/#');
-    mw('test/test/test/test', 'muh/#');
-}).add('old wildcard mismatch #', function () {
-    mw2('test/test/test/test', 'test/test/muh/#');
-    mw2('test/test/test/test', 'test/muh/#');
-    mw2('test/test/test/test', 'muh/#');
-    mw2('test/test/test/test', 'muh/#');
-}).add('new wildcard match + #', function () {
-    mw('test/test/test/test', 'test/+/test/#');
-    mw('test/test/test/test', 'test/+/#');
-    mw('test/test/test/test', 'test/+/+/#');
-    mw('test/test/test/test', 'test/test/+/#');
-}).add('old wildcard match + #', function () {
-    mw2('test/test/test/test', 'test/+/test/#');
-    mw2('test/test/test/test', 'test/+/#');
-    mw2('test/test/test/test', 'test/+/+/#');
-    mw2('test/test/test/test', 'test/test/+/#');
-}).add('new wildcard mismatch + #', function () {
-    mw('test/test/test/test', 'test/+/muh/#');
-    mw('test/test/test/test', 'test/muh/+/#');
-    mw('test/test/test/test', 'muh/+/#');
-    mw('test/test/test/test', 'muh/+/#');
-}).add('old wildcard mismatch + #', function () {
-    mw2('test/test/test/test', 'test/+/muh/#');
-    mw2('test/test/test/test', 'test/muh/+/#');
-    mw2('test/test/test/test', 'muh/+/#');
-    mw2('test/test/test/test', 'muh/+/#');
-})
+const wins = {
+    'mqtt-wildcard': 0,
+    'mqtt-match': 0,
+    'mqtt-pattern': 0
+};
 
-// add listeners
-.on('cycle', function(event) {
-    console.log(String(event.target));
-})
-.on('complete', function() {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-// run async
-.run({ 'async': true });
+function bench(desc, pattern, topic) {
+    const suite = new Benchmark.Suite;
+    suite.add('mqtt-wildcard - ' + desc, function () {
+        mw(topic, pattern);
+    }).add('mqtt-pattern  - ' + desc, function () {
+        mqttPattern(pattern, topic);
+    }).add('mqtt-match    - ' + desc, function () {
+        mqttMatch(pattern, topic);
+    }).on('cycle', function (event) {
+        console.log(String(event.target));
+    }).on('complete', function () {
+        const id = this.filter('fastest').map('name')[0].split(' ')[0];
+        console.log(' => Fastest is ' + id + '\n');
+        wins[id] += 1;
+    }).run({'async': false});
+}
+
+bench('match equal depth 4', 'test/test/test/test', 'test/test/test/test');
+bench('match equal depth 8', 'test/test/test/test/test/test/test/test', 'test/test/test/test/test/test/test/test');
+bench(
+    'match equal depth 16',
+    'test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test',
+    'test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test'
+);
+
+bench('match + 4  1', 'test/test/test/+', 'test/test/test/test');
+bench('match + 4  2', 'test/test/+/test', 'test/test/test/test');
+bench('match + 4  3', 'test/test/+/+', 'test/test/test/test');
+bench('match + 4  4', 'test/+/test/test', 'test/test/test/test');
+bench('match + 4  5', 'test/+/test/+', 'test/test/test/test');
+bench('match + 4  6', 'test/+/+/test', 'test/test/test/test');
+bench('match + 4  7', 'test/+/+/+', 'test/test/test/test');
+bench('match + 4  8', '+/test/test/test', 'test/test/test/test');
+bench('match + 4  9', '+/test/test/+', 'test/test/test/test');
+bench('match + 4 10', '+/test/+/test', 'test/test/test/test');
+bench('match + 4 11', '+/test/+/+', 'test/test/test/test');
+bench('match + 4 12', '+/+/test/test', 'test/test/test/test');
+bench('match + 4 13', '+/+/test/+', 'test/test/test/test');
+bench('match + 4 14', '+/+/+/test', 'test/test/test/test');
+bench('match + 4 15', '+/+/+/+', 'test/test/test/test');
+
+bench('match # 4  1', '#', 'test/test/test/test');
+bench('match # 4  2', 'test/#', 'test/test/test/test');
+bench('match # 4  3', 'test/test/#', 'test/test/test/test');
+bench('match # 4  4', 'test/test/test/#', 'test/test/test/test');
+
+bench('match +# 4  2', 'test/test/+/#', 'test/test/test/test');
+bench('match +# 4  4', 'test/+/test/#', 'test/test/test/test');
+bench('match +# 4  6', 'test/+/+/#', 'test/test/test/test');
+bench('match +# 4  8', '+/test/test/#', 'test/test/test/test');
+bench('match +# 4  10', '+/test/+/#', 'test/test/test/test');
+bench('match +# 4  12', '+/+/test/#', 'test/test/test/test');
+bench('match +# 4  14', '+/+/+/#', 'test/test/test/test');
+
+bench('mismatch + 4  1', 'test/test/muh/+', 'test/test/test/test');
+bench('mismatch + 4  4', 'test/+/test/muh', 'test/test/test/test');
+bench('mismatch + 4  5', 'test/+/muh/+', 'test/test/test/test');
+bench('mismatch + 4  6', 'test/+/+/muh', 'test/test/test/test');
+bench('mismatch + 4  2', 'test/muh/+/test', 'test/test/test/test');
+bench('mismatch + 4  3', 'test/muh/+/+', 'test/test/test/test');
+bench('mismatch + 4  7', 'muh/+/+/+', 'test/test/test/test');
+
+bench('mismatch +# 4  4', 'test/+/muh/#', 'test/test/test/test');
+bench('mismatch +# 4  8', '+/test/muh/#', 'test/test/test/test');
+bench('mismatch +# 4  12', '+/+/muh/#', 'test/test/test/test');
+bench('mismatch +# 4  2', 'test/muh/+/#', 'test/test/test/test');
+bench('mismatch +# 4  10', '+/muh/+/#', 'test/test/test/test');
+bench('mismatch +# 4  6', 'muh/+/+/#', 'test/test/test/test');
+
+console.dir(wins);
